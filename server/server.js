22dee69d4bf5105cbf1b676d5e0d16b06e43c395
dkarnati1 <deejay.karn33@gmail.com>
2017-05-07 21:43:05 -0400

    Test get todos

--- server/server.js
+++ server/server.js
@@ -21,6 +21,18 @@ res.status(200).send(doc);
 res.status(400).send(e);
 });
 });
+
+app.get('/todos',(req,res)=>{
+
+Todo.find().then((todos)=>{
+res.send({todos});
+
+},(e)=>{
+  res.status(400).send(e);
+
+});
+
+});
 app.listen(3000,()=>{
 
 console.log('Started on port 3000');
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,9 +4,22 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo}  = require('./../models/todo');
 
+const todos = [{
+text:'First test todo'
+
+},{
+
+text:'Second test todo'
+
+}];
+
+
 beforeEach((done)=>{
 
-Todo.remove({}).then(()=>done());
+Todo.remove({}).then(()=>{
+
+  return Todo.insertMany(todos);
+}).then(()=>done());
 
 });
 
@@ -30,7 +43,7 @@ if(err){
   return done(err);
 }
 
-Todo.find().then((todos)=>{
+Todo.find({text}).then((todos)=>{
 
   expect(todos.length).toBe(1);
   expect(todos[0].text).toBe(text);
@@ -46,7 +59,6 @@ request(app)
 .expect(400)
 .expect((res)=> {
   expect(res.body.errors.text.message).toBe('Path `text` is required.');
-
 })
 .end((err,res) =>{
 
@@ -57,10 +69,26 @@ request(app)
 
 Todo.find().then((todos)=>{
 
-  expect(todos.length).toBe(0);
+  expect(todos.length).toBe(2);
   done();
 }).catch((e)=>done(e));
 });
 });
 
 });
+
+describe('GET /todos',()=>{
+
+it('should get all todos', (done)=>{
+request(app)
+.get('/todos')
+.expect(200)
+.expect((res)=>{
+expect(res.body.todos.length).toBe(2);
+
+})
+.end(done);
+})
+
+
+})
